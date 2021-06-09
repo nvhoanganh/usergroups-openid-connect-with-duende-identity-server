cbb3116b30c6d95c049a90298d2a9110f12c08f5
Anthony Nguyen <nvhoanganh1909@gmail.com>
2021-06-09 20:03:15 +1000

    use ENV file

--- source_code/react-client/publish-pact.js
+++ source_code/react-client/publish-pact.js
@@ -1,14 +1,16 @@
+require("dotenv").config();
 var pact = require("@pact-foundation/pact-node");
+
 var opts = {
 	pactFilesOrDirs: [`../pacts`],
-	pactBroker: "https://ssw.pactflow.io",
+	pactBroker: process.env.PACT_SERVER,
 	// this can be the fix version (e.g. release version)
 	// or this could be git commit SHA
-  consumerVersion: "1.0.1.103",
-  pactBrokerToken: "xRy0RdbaDE1oSvyxEfuT6Q", 
-	// which environemtn is this? 
-	tags: ["test","prod"],
-  // verbose: true
+	consumerVersion: "1.0.1.103",
+	pactBrokerToken: process.env.PACT_SERVER_TOKEN,
+	// which environemtn is this?
+	tags: ["test", "prod"],
+	// verbose: true
 };
 
 pact.publishPacts(opts).then(function () {
